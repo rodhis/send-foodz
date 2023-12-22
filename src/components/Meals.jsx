1b9e0b8dd4db7a6e05334ee3eadfff6278cd9452
rodhis <rodrigonoliva@gmail.com>
2023-12-22 15:48:50 -0300

    added custom hook for fetching and sending data

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,27 +1,21 @@
-import { useState, useEffect } from "react"
 import MealItem from "./MealItem.jsx"
+import useHttp from "../hooks/useHttp.js"
+
+const requestConfig = {}
 
 export default function Meals() {
 
-    const [loadedMeals, setLoadedMeals] = useState([])
+    const {
+        data: loadedMeals, 
+        isLoading, 
+        error,
+    } = useHttp('http://localhost:3000/meals', requestConfig, [])
 
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals')
-     
-            if (!response.ok) {
-                //...
-            }
-     
-            const meals = await response.json()
-            setLoadedMeals(meals)
-         }
+    console.log(loadedMeals);
 
-         fetchMeals()
-    }, [])
-
-    
-
+    if (isLoading) {
+        return <p className="center">Fetching meals...</p>
+    }
 
     return (
         <ul id="meals">
